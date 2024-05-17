5a7d9a1bd179365272ac08fa919eeea6cc13fba7
lovehunter9 <wangrx07@aliyun.com>
2024-05-17 14:24:03 +0800

    bugfix: change libfile copy logic to fit both cloud and minibox

--- docker3.sh
+++ docker3.sh
@@ -8,6 +8,15 @@
 #cd ..
 #cd ..
 
+if [ ! -f "/usr/lib/x86_64-linux-gnu/libcuda.so.1" ]; then
+  cp /libcuda.so.1 /usr/lib/x86_64-linux-gnu/ && \
+  rm -f usr/lib/x86_64-linux-gnu/libcudart.so.12 && \
+  rm -f usr/lib/x86_64-linux-gnu/libcublas.so.12 && \
+  rm -f usr/lib/x86_64-linux-gnu/libcublasLt.so.12
+fi
+
+rm -f /libcuda.so.1
+
 curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install_v2.sh | bash -s
 source $HOME/.bashrc
 
