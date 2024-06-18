8934451e14ee0256a9e48ac1e6271023988af22a
lovehunter9 <wangrx07@aliyun.com>
2024-06-18 17:03:27 +0800

    arm64 docker compile

--- docker3.sh
+++ docker3.sh
@@ -8,11 +8,31 @@
 #cd ..
 #cd ..
 
-if [ ! -f "/usr/lib/x86_64-linux-gnu/libcuda.so.1" ]; then
-  cp /libcuda.so.1 /usr/lib/x86_64-linux-gnu/ && \
-  rm -f usr/lib/x86_64-linux-gnu/libcudart.so.12 && \
-  rm -f usr/lib/x86_64-linux-gnu/libcublas.so.12 && \
-  rm -f usr/lib/x86_64-linux-gnu/libcublasLt.so.12
+#if [ ! -f "/usr/lib/x86_64-linux-gnu/libcuda.so.1" ]; then
+#  cp /libcuda.so.1 /usr/lib/x86_64-linux-gnu/ && \
+#  rm -f usr/lib/x86_64-linux-gnu/libcudart.so.12 && \
+#  rm -f usr/lib/x86_64-linux-gnu/libcublas.so.12 && \
+#  rm -f usr/lib/x86_64-linux-gnu/libcublasLt.so.12
+#fi
+
+# 检测 CPU 架构
+ARCH=$(uname -m)
+
+if [ "$ARCH" == "x86_64" ]; then
+  LIBDIR="/usr/lib/x86_64-linux-gnu"
+elif [ "$ARCH" == "aarch64" ]; then
+  LIBDIR="/usr/lib/aarch64-linux-gnu"
+else
+  echo "Unsupported CPU architecture: $ARCH"
+  exit 1
+fi
+
+# 复制 libcuda.so.1
+if [ ! -f "$LIBDIR/libcuda.so.1" ]; then
+  cp /libcuda.so.1 "$LIBDIR/" && \
+  rm -f "$LIBDIR/libcudart.so.12" && \
+  rm -f "$LIBDIR/libcublas.so.12" && \
+  rm -f "$LIBDIR/libcublasLt.so.12"
 fi
 
 rm -f /libcuda.so.1
